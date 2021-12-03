991370f1a57a0b7371d3bd96c95d2876a0e49cf9
Krystian <Krystian.Dutkait@gmail.com>
2021-12-03 17:00:39 +0100

    Add input to enter your own numbers

--- arithmetic&comparison&logical operators/index.php
+++ arithmetic&comparison&logical operators/index.php
@@ -9,12 +9,26 @@
 <body>
     <style> *{font-family: sans-serif; text-align: center; font-size: 35px; background-color: black;color: whitesmoke;} </style>
         <h2> Arithmetic operators</h2>
-        
+        <form method="post">  
+    Enter First Number (a) :
+    <input type="number" name="number1" /><br><br>  
+    Enter Second Number (b) : 
+    <input type="number" name="number2" /><br><br>  
+    <input  type="submit" name="submit" value="Add">  
+</form>  
+<?php  
+        $number1 = 1;
+        $number2 = 1;
+    if(isset($_POST['submit']))  {  
+        $number1 = $_POST['number1'];  
+        $number2 = $_POST['number2'];  
+    }  
+?>  
      
      <?php 
      
-        $a = 10;
-        $b = 7;
+        $a = $number1;
+        $b = $number2;
         echo "a = $a, b = $b<br>";
         echo "<br>Addition = " . ($a + $b);
         echo "<br>Subtraction = " . ($a - $b);
@@ -30,7 +44,7 @@
         echo "<br>a=" . ++$a;
         
         echo "<br>Exponentiation = " . POW($a,$b);
-        echo "<br>Square rooting = " . sqrt($a);
+        echo "<br>Square rooting = " . round(sqrt($a),2);
         
      ?>
     
