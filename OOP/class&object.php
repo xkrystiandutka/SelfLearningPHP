e2fd49a2f38a36101fc5c421e4587636e23e03cd
Krystian <Krystian.Dutkait@gmail.com>
2021-12-11 16:35:31 +0100

    Code refactoring by add main.css
    Create one css file to reduce repetition in the code.

--- OOP/class&object.php
+++ OOP/class&object.php
@@ -4,31 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
     <title>Class & Object</title>
 </head>
 <body>
     <h1>Class & Object</h1>
-<style>
-        * {
-            font-family: sans-serif;
-            text-align: center;
-            font-size: 35px;
-        }
-        
-        table{
-            display: inline-block;
-
-        }
-
-        th,td {
-            border: 1px solid #999;
-            border-collapse: collapse;
-            padding: 5px;
-        }
-</style>
-<?php
-
-?>
 <table>
     <tr>
       <td>
