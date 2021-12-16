eb97b854e30f37cb118d901b8aa7592cc7200797
Krystian <Krystian.Dutkait@gmail.com>
2021-12-16 18:42:18 +0100

    Random words generate

--- Projects/passwordGenerator/passwordCreator.php
+++ Projects/passwordGenerator/passwordCreator.php
@@ -1,8 +1,14 @@
 <?php
 
 class passwordCreator {
+    
+    const LEVEL_EASY = 'easy';
+    const LEVEL_MEDIUM = 'medium';
+    const LEVEL_HARD = 'hard';
+
     private $type = "hard";
     private $limit = 7;
+    private $easyWords = "cow, dog, fish, fly, fox, moon, bird, car";
 
     public function __construct($type, $limit)
     {
@@ -11,7 +17,27 @@ class passwordCreator {
     }
 
     public function generate(){
-        return "My new password";
+        if($this->type == self::LEVEL_EASY){
+            return $this -> createEasyPassword();
+        }else if($this->type == self::LEVEL_MEDIUM){
+
+        }else if($this->type == self::LEVEL_HARD){
+        
+        }
+        return "An error occurred";
+    }
+
+    private function createEasyPassword(){
+        $pass = "";
+        $word = $this->getRandomEasyWord();
+        return $word;
+
+    }
+
+    private function getRandomEasyWord(){
+        $ewArray = explode(',', $this->easyWords);
+        $key = array_rand($ewArray, 1);
+        return trim($ewArray[$key]);
     }
 }
 
