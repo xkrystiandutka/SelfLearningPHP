2f64d696d151900d2c128ba3c5d056b3178702ec
Krystian <Krystian.Dutkait@gmail.com>
2021-12-17 10:49:11 +0100

    tic tac toe template

--- Projects/ticTacToe/index.php
+++ Projects/ticTacToe/index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +13,81 @@
     <title>Tic Tac Toe</title>
 </head>
 <body>
-    
+    <style>
+        .tct-table {
+            border-collapse: separate;
+        }
+    </style>
+    <h1>Tic Tac Toe Games</h1>
+
+    <table class="tct-table" border="1">
+        <tr>
+            <td>
+                <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select>
+            </td>
+            <td>
+                <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select></td>
+            <td>
+            <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select>
+            </td>
+            <td>
+                <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select></td>
+            <td>
+            <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select>
+            </td>
+        </tr>        <tr>
+            <td>
+                <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select>
+            </td>
+            <td>
+                <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select></td>
+            <td>
+            <select>
+                    <option>select</option>
+                    <option>O</option>
+                    <option>X</option>
+                </select>
+            </td>
+        </tr>
+    </table>
+
+
 </body>
 </html>
